2221804c1b0a69b0c2f5363259e6ad67e403fffe
zhang4417 <1804815164@qq.com>
2019-11-17 18:22:49 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,35 @@
+window.dom = {
+    find(selector, scope) {
+        return (scope || document).querySelectorAll(selector)
+    },
+    style(node, name, value) {
+        if (arguments.length === 3) {
+            node.style[name] = value
+        } else if (arguments.length === 2) {
+            if (typeof name === 'string') {
+                return node.style[name]
+            } else if (name instanceof Object) {
+                for (let key in name) {
+                    return node.style[key] = name[key]
+                }
+            }
+        }
+    },
+    each(nodeList, fn) {
+        for (let i = 0; i < nodeList.length; i++) {
+            fn(nodeList[i])
+            //fn.call(null,nodeList[i])
+        }
+    }
+}
+
+let div = dom.find('#test>.red')[0]
+console.log(div)
+dom.style(div, 'color', 'red')
+const divList = dom.find('.red')
+dom.each(divList, (n) => console.log(n))
+// const div = dom.find('#test>.red')[0] // 获取对应的元素
+// dom.style(div, 'color', 'red') // 设置 div.style.color
+
+// const divList = dom.find('.red') // 获取多个 div.red 元素
+// dom.each(divList, (n) => console.log(n)) // 遍历 divList 里的所有元素
